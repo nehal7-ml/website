d95e3aaea344a84255a3b8e8213be4e6a71cf811
nehal7-ml <nehal.sk.99@gmail.com>
2025-05-16 16:17:29 +0530

    added redirect to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import tailwind from "@astrojs/tailwind";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
+
+
+
 export default defineConfig({
+  redirects: {
+    '/': "/coming-soon"
+  },
 
   vite: {
     plugins: [tailwindcss()]
