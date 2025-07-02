c64690569a0533b901c72ed30e69bcd9431b85fd
nehal7-ml <nehal.sk.99@gmail.com>
2025-07-02 20:15:44 +0530

    use loading component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
 import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 
-
-
 export default defineConfig({
-  redirects: {
-    '/': "/coming-soon"
-  },
-
   vite: {
     plugins: [tailwindcss()]
   }
